0f97d31be028f6457b27a58f2a6481e737e37344
Edward Rodriquez <Ed.R.Rodriquez@gmail.com>
2020-12-13 18:46:25 -0600

    Created greeting model and greetingcontroller for localhost:8080/greeting to output JSON format data

---
+++ API/src/main/java/TeamMcDev/project/Models/Greeting.java
@@ -0,0 +1,21 @@
+package TeamMcDev.project.Models;
+
+public class Greeting {
+    //Greeting class will provide structure for JSON data to be returned to user; ID data and a basic greeting.
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
