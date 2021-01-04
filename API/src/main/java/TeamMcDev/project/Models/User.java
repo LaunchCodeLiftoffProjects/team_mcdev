8a878bb3a9bd1c287f6ae43f00d14918d5e8bdbe
thelonewolf84 <perrystroud@yahoo.com>
2021-01-04 17:50:16 -0600

    added name field to user model

--- API/src/main/java/TeamMcDev/project/Models/User.java
+++ API/src/main/java/TeamMcDev/project/Models/User.java
@@ -3,12 +3,17 @@ package TeamMcDev.project.Models;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class User extends AbstractEntity{
+    @NotBlank(message = "Name can not be blank!")
+    @Size(min = 1, max = 100)
+    private String name;
 
     @NotNull
     private String email;
@@ -20,9 +25,19 @@ public class User extends AbstractEntity{
     @JoinColumn
     private final List<Recipe> jobs = new ArrayList<>();
 
-    public User(@NotNull String email, @NotNull String password) {
+    public User(String name, String email, String password) {
+        this.name = name;
         this.email = email;
         this.password = password;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
